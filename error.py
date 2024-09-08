ae947cd74ac1196bb622aaae62a293d733d97bc1
FranciscoVega <pancho.vega.90@gmail.com>
2024-09-08 19:59:58 -0300

    last

--- error.py
+++ error.py
@@ -2,8 +2,6 @@ class Error(Exception):
     pass
 
 class LargoExcedidoError(Error):
-    def __init__(self, mensaje:str) -> None:
-        self.__mensaje = mensaje
     pass
 
 class SubTipoInvalidoError(Error):
